d497c499948d7155e17be5f23fbb08e51974c62d
Ted Zilist <tzilist@gmail.com>
2017-06-17 20:06:40 -0700

    fixed, pheew! :D

--- web/services/reservation_service.ex
+++ web/services/reservation_service.ex
@@ -4,16 +4,10 @@ defmodule HutchCalendar.ReservationService do
   alias HutchCalendar.Repo
 
   def query_availability(time_start, time_end, room_id) do
-    IO.inspect time_start
-    IO.inspect time_end
-    {:ok, time_start} = NaiveDateTime.from_iso8601(time_start)
-    {:ok, time_start} = Ecto.DateTime.cast(time_start)
-    {:ok, time_end} = NaiveDateTime.from_iso8601(time_end)
-    {:ok, time_end} = Ecto.DateTime.cast(time_end)
     query = from res in "reservations",
-      where: res.conference_room_id == ^room_id and res.time_start < type(^time_end, Ecto.DateTime) and res.time_end >= type(^time_end, Ecto.DateTime),
-      or_where: res.id == ^room_id and res.time_end > type(^time_start, Ecto.DateTime) and res.time_start < type(^time_start, Ecto.DateTime),
-      or_where: res.id == ^room_id and res.time_start <= type(^time_start, Ecto.DateTime) and res.time_end >= type(^time_end, Ecto.DateTime),
+      where: res.conference_room_id == ^room_id and res.time_start < type(^time_end, :utc_datetime) and res.time_end > type(^time_end, :utc_datetime),
+      or_where: res.conference_room_id == ^room_id and res.time_end > type(^time_start, :utc_datetime) and res.time_start <= type(^time_start, :utc_datetime),
+      or_where: res.conference_room_id == ^room_id and res.time_start <= type(^time_start, :utc_datetime) and res.time_end >= type(^time_end, :utc_datetime),
       select: res.id
 
     Repo.all(query)
